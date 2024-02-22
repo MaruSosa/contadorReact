3219820d5f2662fb9c949f3f92f6ad3ece6a8efa
mariana <marianass712@gmail.com>
2024-02-21 21:19:40 -0300

    agregamos un props en el componente informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Informacion from "./components/Informacion";
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
+  const anioActual=2023;
   return (
-    <section>
-    <h1 className="display-3 text-center">Prueba</h1>
-    <Informacion></Informacion>
-    <Informacion/>
+    <section className="container">
+    <h1 className="display-3 text-center">Proyecto de React</h1>
+    {/* <Informacion></Informacion> */}
+    <Informacion comision='c76i' anioActualProps={anioActual}/>
   </section>
   );
 
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,16 +1,16 @@
-const Informacion = () => {
+const Informacion = (props) => {
   //aqui va la logica del componente
   return (
     //puedo poner un poco de logica
     <>
-      <h2>Conceptos de React</h2>
+      <h2>Conceptos de React {props.comision}</h2>
       <section>
         {/*aqui va todo el maquetado */}
         <p>
           React es una biblioteca de JavaScript desarrollada por Facebook que se
           utiliza para construir interfaces de usuario interactivas en
           aplicaciones web. Fue lanzada en 2013 y se ha vuelto muy popular en la
-          comunidad de desarrollo web.{" "}
+          comunidad de desarrollo web. <br/> comision:{props.anioActualProps}
         </p>
       </section>
     </>
