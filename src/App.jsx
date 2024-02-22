217acd688e85f6a96f40377eda6c24bc344b95fa
mariana <marianass712@gmail.com>
2024-02-21 21:29:45 -0300

    agregamos el maquetado del componente contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Informacion from "./components/Informacion";
+import Contador from "./components/Contador";
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   const anioActual=2023;
@@ -7,6 +8,7 @@ function App() {
     <h1 className="display-3 text-center">Proyecto de React</h1>
     {/* <Informacion></Informacion> */}
     <Informacion comision='c76i' anioActualProps={anioActual}/>
+    <Contador/>
   </section>
   );
 
